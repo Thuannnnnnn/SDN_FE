e1de0f34076360abab29b6b52efaeaf6f0ffb8af
Thuannnnnnn <tranquocthuan2003@gmail.com>
2024-09-19 17:21:07 +0700

    commit

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Widgets = React.lazy(() => import('./views/widgets/Widgets'))
-const course = React.lazy(() => import('./views/course/CourseList'))
+const courseList = React.lazy(() => import('./views/course/CourseList'))
+const courseAdd = React.lazy(() => import('./views/course/CourseAdd'))
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/widgets', name: 'Widgets', element: Widgets },
-  { path: '/course', name: 'course', element: course },
+  { path: '/course', name: 'Course', element: courseList },
+  { path: '/course_add', name: 'Course', element: courseAdd },
 ]
 
 export default routes
